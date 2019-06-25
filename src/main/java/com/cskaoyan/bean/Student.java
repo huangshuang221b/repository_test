e589a2886ce6df8d0b7de47b60b30663b43e9fb2
huangshuang221b <1700890377@qq.com>
2019-06-25 22:25:08 +0800

    first change test by hs 190625

---
+++ src/main/java/com/cskaoyan/bean/Student.java
@@ -0,0 +1,22 @@
+package com.cskaoyan.bean;
+
+public class Student {
+    int stu_id;
+    String stu_name;
+
+    public int getStu_id() {
+        return stu_id;
+    }
+
+    public void setStu_id(int stu_id) {
+        this.stu_id = stu_id;
+    }
+
+    public String getStu_name() {
+        return stu_name;
+    }
+
+    public void setStu_name(String stu_name) {
+        this.stu_name = stu_name;
+    }
+}
